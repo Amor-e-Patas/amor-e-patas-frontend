1b55c362f3e7dc23cd4f7ae3bc04ab198696b557
ana.santana <anaflavia2309@gmail.com>
2021-10-08 23:06:02 -0300

    Adicionando verificações e correções a notícia

--- pages/noticia/[id_post].tsx
+++ pages/noticia/[id_post].tsx
@@ -1,227 +1,299 @@
-import Navbar from "../../components/Navbar"
+import Navbar from "../../components/Navbar";
 import Footer from "../../components/Footer";
-import React, { useState, useEffect, ChangeEvent } from "react";
+import React, { useState, useEffect, ChangeEvent, useContext } from "react";
 import styles from "../../styles/components/FormNoticia.module.css";
 import { useRouter } from "next/router";
 import VerifyAuth from "../../components/verifyAuth";
-import { criarImgPost, criarPost, getAssuntos, getPost } from "../../services/post";
+import {
+  criarImgPost,
+  criarPost,
+  getAssuntos,
+  getPost,
+} from "../../services/post";
 import { getAssunto } from "../../services/post";
-import moment from 'moment';
-import { GetStaticPaths, GetStaticProps, GetStaticPropsContext, InferGetStaticPropsType } from "next";
-import { criarComentario, deleteComentario, getComentarios } from "../../services/comentario";
+import moment from "moment";
+import {
+  GetStaticPaths,
+  GetStaticProps,
+  GetStaticPropsContext,
+  InferGetStaticPropsType,
+} from "next";
+import {
+  criarComentario,
+  deleteComentario,
+  getComentarios,
+} from "../../services/comentario";
+import { AuthContext } from "../../contexts/auth";
 
 interface Assunto {
-    id_assunto: number,
-    nome_ass: string
+  id_assunto: number;
+  nome_ass: string;
 }
 
 interface Post {
-    id_post: number;
-    titulo: string,
-    corpo: string;
-    autor: string,
-    data: string,
-    assuntos: Array<Assunto>,
-    images: Array<{
-        filepath: string;
-    }>,
+  id_post: number;
+  titulo: string;
+  corpo: string;
+  autor: string;
+  data: string;
+  assuntos: Array<Assunto>;
+  images: Array<{
+    filepath: string;
+  }>;
 }
 
 interface Comentario {
-    texto: string,
-    data: string,
-    id_comentario: number,
-    nome_usu: string,
-    id_post: number
+  texto: string;
+  data: string;
+  id_comentario: number;
+  nome_usu: string;
+  id_post: number;
+  id_usuario: number;
 }
 
 export const getStaticPaths: GetStaticPaths<{ slug: string }> = async () => {
-
-    return {
-        paths: [],
-        fallback: 'blocking'
-    }
-}
+  return {
+    paths: [],
+    fallback: "blocking",
+  };
+};
 
 export const getStaticProps: GetStaticProps = async (
-    context: GetStaticPropsContext
+  context: GetStaticPropsContext
 ) => {
+  return {
+    props: {
+      id_post: context.params?.id_post,
+    },
+  };
+};
 
-    return {
-        props: {
-            id_post: context.params?.id_post
-        },
-    }
-}
+export default function Usuario({
+  id_post,
+}: InferGetStaticPropsType<typeof getStaticProps>) {
+  const [titulo, setTitulo] = useState("");
+  const [corpo, setCorpo] = useState("");
+  const [autor, setAutor] = useState("");
+  const [data, setData] = useState("");
+  const router = useRouter();
+  const [assuntos, setAssuntos] = useState(Array<Assunto>());
+  const [selectAssunto, setSelectAssunto] = useState(Array<Number>());
+  const [images, setImages] = useState<File[]>([]);
+  const [previewImages, setPreviewImages] = useState<string[]>([]);
+  const [post, setPosts] = useState<Post>();
+  const [assuntoT, setAssuns] = useState(Array<Assunto>());
+  const [comentarios, setComentarios] = useState(Array<Comentario>());
+  const [texto, setTexto] = useState("");
+  const [id_usuarioComentario, setUsuario] = useState("");
+  const [id_comentario, setIdComentario] = useState(0);
+  const [dataComent, setDataComent] = useState(moment().format("YYYY-MM-DD"));
+  const [mostrarModal, setMostrarModal] = useState(false);
+  const { id_usuario, role } = useContext(AuthContext);
 
-export default function Usuario({ id_post }: InferGetStaticPropsType<typeof getStaticProps>) {
-    const [titulo, setTitulo] = useState("");
-    const [corpo, setCorpo] = useState("");
-    const [autor, setAutor] = useState("");
-    const [data, setData] = useState("");
-    const router = useRouter();
-    const [assuntos, setAssuntos] = useState(Array<Assunto>());
-    const [selectAssunto, setSelectAssunto] = useState(Array<Number>());
-    const [images, setImages] = useState<File[]>([]);
-    const [previewImages, setPreviewImages] = useState<string[]>([]);
-    const [post, setPosts] = useState<Post>();
-    const [assuntoT, setAssuns] = useState(Array<Assunto>());
-    const [comentarios, setComentarios] = useState(Array<Comentario>());
-    const [texto, setTexto] = useState("");
-    const [id_usuario, setUsuario] = useState("");
-    const [dataComent, setDataComent] = useState(moment().format("YYYY-MM-DD"));
-    const [mostrarModal, setMostrarModal] = useState(false);
+  useEffect(() => {
+    async function fetchAPI() {
+      try {
+        const post = (await getPost(id_post)) as Post;
 
-    useEffect(() => {
-        async function fetchAPI() {
-            try {
+        setPosts(post);
+        setAutor(post.autor);
+        setTitulo(post.titulo);
+        setCorpo(post.corpo);
+        setData(post.data);
 
-                const post = await getPost(id_post) as Post;
+        //const assunto = await getAssunto() as Assunto;
+        setAssuntos(post.assuntos);
+        //setAssuns(post.assuntos);
 
-                setPosts(post);
-                setAutor(post.autor);
-                setTitulo(post.titulo);
-                setCorpo(post.corpo);
-                setData(post.data);
+        const assun = post.assuntos.map((assun) => assun.id_assunto);
 
-                //const assunto = await getAssunto() as Assunto;
-                setAssuntos(post.assuntos);
-                //setAssuns(post.assuntos);
+        console.log(post, "caract");
+        setSelectAssunto(assun);
 
-                const assun = post.assuntos.map(assun => assun.id_assunto)
-
-                console.log(post, 'caract')
-                setSelectAssunto(assun);
-
-                const comentario = await getComentarios(id_post);
-                setComentarios(comentario);
-                console.log(comentario);
-
-            } catch (err) {
-                console.log(err);
-            }
-        }
-
-        fetchAPI();
-    }, []);
-
-    async function eventoCriarPost() {
-
-        if (texto == "") {
-            alert("Comentário vazio");
-            return;
-        }
-
-        try {
-            await
-                criarComentario(texto,
-                    dataComent,
-                    parseInt(id_usuario),
-                    id_post);
-            const comentario = await getComentarios(id_post);
-            setComentarios(comentario);
-            setTexto("");
-        } catch (error) {
-            alert("Erro ao criar comentário.")
-        }
+        const comentario = await getComentarios(id_post);
+        setComentarios(comentario);
+        console.log(comentario);
+      } catch (err) {
+        console.log(err);
+      }
     }
 
-    async function eventoGetComentario() {
+    fetchAPI();
+  }, []);
 
-        try {
-            const comentario = await getComentarios(id_post);
-            setComentarios(comentario);
-        } catch (error) {
-            alert("Erro ao retornar comentário.")
-        }
-
+  async function eventoCriarPost() {
+    if (texto == "") {
+      alert("Comentário vazio");
+      return;
     }
-    return (
-        <>
-            <VerifyAuth />
-            <div><Navbar /></div>
-            <div>
-                <div className={styles.imagesContainer}>
-                    <img src={`http://localhost:3333/${post?.images[0].filepath}`} className={styles.imagem} alt="" />
-                </div>
+
+    try {
+      await criarComentario(
+        texto,
+        dataComent,
+        parseInt(id_usuarioComentario),
+        id_post
+      );
+      const comentario = await getComentarios(id_post);
+      setComentarios(comentario);
+      setTexto("");
+    } catch (error) {
+      alert("Erro ao criar comentário.");
+    }
+  }
+
+  async function eventoGetComentario() {
+    try {
+      const comentario = await getComentarios(id_post);
+      setComentarios(comentario);
+    } catch (error) {
+      alert("Erro ao retornar comentário.");
+    }
+  }
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      <div>
+        <div className={styles.imagesContainer}>
+          <img
+            src={`http://localhost:3333/${post?.images[0].filepath}`}
+            className={styles.imagem}
+            alt=""
+          />
+        </div>
+      </div>
+      <body className={styles.body}>
+        <div className={styles.container}>
+          <div className={styles.temperamento}>
+            <div className={styles.temp}>
+              <table>
+                <tr>
+                  {assuntos.map((assunto) => (
+                    <td>{assunto.nome_ass} </td>
+                  ))}
+                  <td>
+                    <p className={styles.autor}>
+                      {" "}
+                      | {moment(data).format("DD/MM/YYYY")}{" "}
+                    </p>
+                  </td>
+                </tr>
+              </table>
             </div>
-            <body className={styles.body}>
-                <div className={styles.container}>
-                    <div className={styles.temperamento}>
-                        <div className={styles.temp}>
-                            <div>
-                                <p className={styles.autor}> ASSUNTO | {data}</p>
-
-                            </div>
-                            <p className={styles.amor}>Mais Detalhes</p>
-                            <ul>
-                                {
-                                    assuntos.map((assunto) =>
-                                        <li>{assunto.nome_ass}</li>
-                                    )
-                                }
-                            </ul>
-                        </div>
-                    </div>
-                    <div>
-                        <div>
-                            <h2 className={styles.titulo}>{titulo}</h2>
-                            <div>
-                                <p className={styles.autor}>Escrito por {autor} </p>
-                            </div>
-                            <div dangerouslySetInnerHTML={{ __html: corpo }} >
-                            </div>
-                        </div>
-                    </div>
-
-                    <div>
-                        <textarea name="comentario" placeholder="Adicionar um comentário..." value={texto} onChange={(e) => setTexto(e.currentTarget.value)} ></textarea>
-                    </div>
-
-                    <input type="submit" className={styles.botaoenviar} value="Comentar" onClick={(e) => {
-                        e.preventDefault()
-                        eventoCriarPost()
-                    }} />
-
-                    <div>
-                        {
-                            comentarios.map((comentario) =>
-                                <div >
-                                    <div>
-                                        <p>Autor: {comentario.nome_usu}</p>
-                                        <p>Comentário: {comentario.texto}</p>
-                                        <p>Data: {comentario.data}</p>
-                                        <hr />
-                                        <div className={styles.modal} style={{ display: mostrarModal ? "block" : "none" }}>
-                                            <p>deseja realmente deletar?
-                                                <button onClick={(e) => {
-                                                    e.preventDefault();
-                                                    deleteComentario(comentario.id_comentario);
-                                                    eventoGetComentario();
-                                                }}> Sim</button>
-                                                <button onClick={(e) => {
-                                                    e.preventDefault()
-                                                    setMostrarModal(false);
-                                                }}> Não</button>
-                                            </p>
-                                        </div>
-                                        <button className={styles.botaoexcluir} value="excluir" onClick={(e) => {
-                                            e.preventDefault()
-                                            setMostrarModal(true);
-                                        }}>Excluir</button>
-                                    </div>
-                                </div>
-                            )
-                        }
-                    </div>
-                </div>
-            </body>
-            <div></div>
+          </div>
+          <div>
             <div>
-                <Footer />
+              <h1 className={styles.titulo}>{titulo}</h1>
+              <div>
+                <p className={styles.autor}>Escrito por {autor} </p>
+              </div>
+              <div dangerouslySetInnerHTML={{ __html: corpo }}></div>
             </div>
+          </div>
+          <br />
+          <div>
+            <hr />
+          </div>
+          <br />
+          {id_usuario != 0 ? (
+            <div>
+              <textarea
+                name="comentario"
+                className={styles.comentario}
+                placeholder="Adicionar um comentário..."
+                value={texto}
+                onChange={(e) => setTexto(e.currentTarget.value)}
+              ></textarea>
+              <input
+                type="submit"
+                className={styles.botaoenviar}
+                value="Comentar"
+                onClick={(e) => {
+                  e.preventDefault();
+                  eventoCriarPost();
+                }}
+              />
+            </div>
+          ) : (
+            <></>
+          )}
+          <br />
 
-        </>
-    )
+          <div>
+            {comentarios.map((comentario) => (
+              <div>
+                <div className={styles.comentariopost}>
+                  <p className={styles.autorcom}>{comentario.nome_usu}</p>
+                  <p className={styles.date}>
+                    {moment(comentario.data).format("DD/MM/YYYY")}
+                  </p>
+                  <p className={styles.comen}> {comentario.texto}</p>
+                  {id_usuario == comentario.id_usuario || role == "admin" ? (
+                    <button
+                      className={styles.botaoexcluir}
+                      value="excluir"
+                      onClick={(e) => {
+                        e.preventDefault();
+                        setIdComentario(comentario.id_comentario);
+                        console.log(comentario, "coem");
+                        setMostrarModal(true);
+                      }}
+                    >
+                      Excluir
+                    </button>
+                  ) : (
+                    <></>
+                  )}
+                </div>
+                <br />
+              </div>
+            ))}
+          </div>
+          <div
+            className={styles.modal}
+            style={{ display: mostrarModal ? "block" : "none" }}
+          >
+            <p>
+              Deseja realmente excluir a notícia?
+              <div className={styles.botoesmold}>
+                <div>
+                  <button
+                    onClick={async (e) => {
+                      e.preventDefault();
+                      await deleteComentario(id_comentario);
+                      setMostrarModal(false);
 
+                      const comentario = await getComentarios(id_post);
+                      setComentarios(comentario);
+                    }}
+                  >
+                    {" "}
+                    Sim
+                  </button>
+                </div>
+                <div>
+                  <button
+                    onClick={(e) => {
+                      e.preventDefault();
+                      setMostrarModal(false);
+                    }}
+                  >
+                    {" "}
+                    Não
+                  </button>
+                </div>
+              </div>
+            </p>
+          </div>
+          <div></div>
+        </div>
+      </body>
+      <div></div>
+      <div>
+        <Footer />
+      </div>
+    </>
+  );
 }
---
+++ pages/noticiasauth.tsx
@@ -0,0 +1,158 @@
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer";
+import React, { useState, useEffect, ChangeEvent } from "react";
+import styles from "../styles/components/FormNoticiasAuth.module.css";
+import { useRouter } from "next/router";
+import VerifyAuth from "../components/verifyAuth";
+import { criarImgPost, deletePost, getAssuntos, getPosts } from "../services/post";
+import { GetStaticPaths, GetStaticProps, GetStaticPropsContext, InferGetStaticPropsType } from "next";
+import moment from 'moment';
+import Link from "next/link";
+
+interface Assunto {
+    id_assunto: number,
+    nome_ass: string
+}
+
+interface Post {
+    id_post: number;
+    titulo: string,
+    corpo: string;
+    autor: string,
+    data: string,
+    assuntos: Array<{
+        id_assunto: string;
+        nome_ass: string;
+    }>,
+    images: Array<{
+        filepath: string;
+    }>,
+}
+
+
+export default function Usuario() {
+    const [titulo, setTitulo] = useState("");
+    const [corpo, setCorpo] = useState("");
+    const [autor, setAutor] = useState("");
+    const [data, setData] = useState("");
+    const router = useRouter();
+    const [assuntos, setAssuntos] = useState(Array<Assunto>());
+    const [selectAssunto, setSelectAssunto] = useState(Array<Number>());
+    const [images, setImages] = useState<File[]>([]);
+    const [previewImages, setPreviewImages] = useState<string[]>([]);
+    const [posts, setPosts] = useState(Array<Post>());
+    const [mostrarModal, setMostrarModal] = useState(false);
+    const [id_post, setIdpost] = useState(0);
+
+    useEffect(() => {
+        async function fetchAPI() {
+            try {
+                const assunto = await getAssuntos();
+                setAssuntos(assunto);
+
+                const post = await getPosts();
+                setPosts(post);
+                console.log(post);
+
+            } catch (err) {
+                console.log(err);
+            }
+        }
+
+        fetchAPI();
+    }, []);
+
+    async function eventoExcluirPost(id_post: number) {
+        try {
+            await deletePost(id_post)
+            const post = await getPosts();
+                setPosts(post);
+        } catch (err) {
+            console.log(err);
+
+        }
+    }
+    return (
+        <>
+            <VerifyAuth />
+            <div><Navbar /></div>
+            <body>
+                <form className={styles.form} name="form">
+                    <div className={styles.container}>
+                        <h3 className={styles.titulo}>Blog</h3>
+                        <div>
+
+                            <div className={styles.mae}>
+                                <div className={styles.containertemp}>
+                                    <div className={styles.temperamento}>
+                                        <div className={styles.temp}>
+                                            <div>
+                                                {
+                                                    posts.map((post) =>
+                                                        <div className={styles.noticias}>
+                                                            <div className={styles.imagesContainer}>
+                                                                <img src={`http://localhost:3333/${post.images[0].filepath}`} className={styles.imagem} alt="" />
+                                                                <hr />
+                                                            </div>
+                                                            <div>
+                                                                <p><Link href={`/noticia/${post.id_post}`}>{post.titulo}</Link>  </p>
+                                                                <p>Autor: {post.autor}</p>
+                                                                <p>Data: {post.data}</p>
+                                                                <div>
+                                                                    <Link href={`/alterarnoticia/${post.id_post}`}><img className={styles.icon} src="/img/editar.png" /></Link>
+                                                                    <Link href={`${post.id_post}`}><span onClick={(e) => {
+                                                                        e.preventDefault()
+                                                                        setIdpost(post.id_post)
+                                                                        setMostrarModal(true)
+                                                                    }}><img className={styles.icon} src="/img/excluir.png" /></span></Link>
+                                                                </div>
+                                                            </div>
+                                                        </div>
+                                                    )
+                                                }
+                                            </div>
+                                            <div className={styles.modal} style={{ display: mostrarModal ? "block" : "none" }}>
+                                                <p>Deseja realmente excluir a notícia?
+                                                    <div className={styles.botoesmold}>
+                                                        <div>
+                                                            <button onClick={(e) => {
+                                                                e.preventDefault()
+                                                                eventoExcluirPost(id_post)
+                                                                setMostrarModal(false);
+                                                            }}> Sim</button>
+                                                        </div>
+                                                        <div>
+                                                            <button onClick={(e) => {
+                                                                e.preventDefault()
+                                                                setMostrarModal(false);
+                                                            }}> Não</button>
+                                                        </div>
+                                                    </div>
+                                                </p>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+
+                            <div className={styles.botoes}>
+                                <input type="submit" className={styles.botaovoltar} value="Voltar" onClick={(e) => {
+                                    e.preventDefault()
+                                }} />
+
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </body>
+
+            <div>
+                <Footer />
+            </div>
+
+        </>
+    )
+
+}
